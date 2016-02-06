17f7e975e00ae832b00d408933c050dcb6b47104
Paweł Sadowski <pawelpiotrsadowski@gmail.com>
2016-02-06 11:50:07 +0100

    .

--- src/WebApplication5/wwwroot/Scripts/index.js
+++ src/WebApplication5/wwwroot/Scripts/index.js
@@ -8,12 +8,46 @@ $(document).on('click touchstart', function (e) {
         $(".navbar-toggle").collapse('hide');
     }
 });
-//$(document).ready(function () {
-//    $(".dropdown").hover(
-//        function () {
-//            $('.dropdown-menu', this).fadeIn("fast");
-//        },
-//        function () {
-//            $('.dropdown-menu', this).fadeOut("fast");
-//        });
-//});
+$(document).ready(function () {
+    //$(".dropdown").hover(
+    //    function () {
+    //        $('.dropdown-menu', this).fadeIn("fast");
+    //    },
+    //    function () {
+    //        $('.dropdown-menu', this).fadeOut("fast");
+    //    });
+    $('.offers-carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+          {
+              breakpoint: 1024,
+              settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+              }
+          },
+          {
+              breakpoint: 600,
+              settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+              }
+          },
+          {
+              breakpoint: 480,
+              settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+              }
+          }
+         
+        ]
+    });
+
+});
+
