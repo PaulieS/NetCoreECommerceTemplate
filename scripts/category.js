0f36e3b6baa9520c52b784491d2f7a107a628aed
PaulieS <pawelpiotrsadowski@gmail.com>
2016-02-23 15:23:51 +0100

    github pages test

---
+++ scripts/category.js
@@ -0,0 +1,21 @@
+﻿$(document).on('click','.big-list',function () {
+    var productsContainer = $('#products-panel');
+    productsContainer.removeClass();
+    productsContainer.addClass('display-rows');
+});
+
+$(document).on('click', '.tiles', function () {
+    var productsContainer = $('#products-panel');
+    productsContainer.removeClass();
+    productsContainer.addClass('display-tiles');
+});
+$(document).on('click', '.small-list', function () {
+    var productsContainer = $('#products-panel');
+    productsContainer.removeClass();
+    productsContainer.addClass('display-small-list');
+});
+$(document).on('click', '.table', function () {
+    var productsContainer = $('#products-panel');
+    productsContainer.removeClass();
+    productsContainer.addClass('display-table');
+});
